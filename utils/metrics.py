d3ca97330c2c03d3fddcb473aa1ca8528f417b70
Krishnakoushik21 <159412979+Krishnakoushik21@users.noreply.github.com>
2025-05-15 11:44:32 +0530

    Create metrics.py

---
+++ utils/metrics.py
@@ -0,0 +1,10 @@
+# utils/metrics.py
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+def compute_metrics(y_true, y_pred):
+    return {
+        "Accuracy": accuracy_score(y_true, y_pred),
+        "Precision": precision_score(y_true, y_pred),
+        "Recall": recall_score(y_true, y_pred),
+        "F1 Score": f1_score(y_true, y_pred),
+    }
